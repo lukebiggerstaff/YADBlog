7b49cbeff5792e3bb2ecf937d5551ba8bd1010d5
luke biggerstaff <luke.biggerstaff@gmail.com>
2018-01-10 10:35:36 -0600

    added a view for replying to another comment. and removed debugging information from post detail page

--- yadblog/blog/views.py
+++ yadblog/blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-from django.urls import reverse, reverse_lazy
+from django.urls import reverse
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import FormView
 from django.utils.timezone import now
@@ -32,9 +32,25 @@ class PostDetailView(DetailView, FormView):
             new_comment = Comment(name=name, email=email, body=body, post_parent=post_parent)
             new_comment.save()
             slug = post_parent.slug
-            print("NAME: {}".format(name))
-            print("EMAIL: {}".format(email))
-            print("BODY: {}".format(body))
-            print("SLUG: {}".format(slug))
-            return HttpResponseRedirect(slug)
+            return HttpResponseRedirect(reverse('post-detail', kwargs={'slug' : slug}))
+
+
+class CommentReplyView(DetailView, FormView):
+    model = Comment
+    form_class = CommentForm
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            body = form.cleaned_data['body']
+            comment_parent = self.get_object()
+            new_comment = Comment(name=name, email=email, body=body, comment_parent=comment_parent)
+            new_comment.save()
+            post_parent = comment_parent.post_parent
+            slug = post_parent.slug
+            return HttpResponseRedirect(reverse('post-detail', kwargs={'slug' : slug}))
+
 
