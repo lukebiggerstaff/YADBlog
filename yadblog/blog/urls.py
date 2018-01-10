9141fb0e0506c99e06be778cb50ebc77e138421d
luke biggerstaff <luke.biggerstaff@gmail.com>
2018-01-10 10:33:20 -0600

    added route to blog/urls.py to add a reply to another comment

--- yadblog/blog/urls.py
+++ yadblog/blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #path('', views.blog_home, name='blog_home')
     path('', views.PostListView.as_view(), name='post-list'),
     path('<slug:slug>', views.PostDetailView.as_view(), name='post-detail'),
+    path('<slug:slug>/<int:pk>', views.CommentReplyView.as_view(), name='comment-reply'),
 ]
