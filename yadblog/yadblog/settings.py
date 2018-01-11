3f677419ccbe367a37a29323819f385b2d4066ef
luke biggerstaff <luke.biggerstaff@gmail.com>
2018-01-11 16:58:21 -0600

    added context_processors.media to the templates setting, and set media_url and media_root settings in settings.py

--- yadblog/yadblog/settings.py
+++ yadblog/yadblog/settings.py
@@ -64,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -120,3 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Media URL and root
+BLOG_DIR = os.path.join(BASE_DIR, 'blog')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BLOG_DIR, 'media')
