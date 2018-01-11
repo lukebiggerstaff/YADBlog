1faa9b4f55cea10b4bf60dfa5365f5077010ede1
luke biggerstaff <luke.biggerstaff@gmail.com>
2018-01-11 16:54:23 -0600

    added a PostImage model to models.py

--- yadblog/blog/models.py
+++ yadblog/blog/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.utils import timezone
 from django.urls import reverse
 
+
+
 # Create your models here.
 
 class Post(models.Model):
@@ -42,3 +44,12 @@ class Comment(models.Model):
 
     def __str__(self):
         return '{}: {}...'.format(self.name, self.body[:10])
+
+
+
+class PostImage(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="blog")
+
+    def __str__(self):
+        return self.name
