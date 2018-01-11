8fc80942c4d6d05b6e1f997b614dfeaae3fd0833
luke biggerstaff <luke.biggerstaff@gmail.com>
2018-01-11 16:57:21 -0600

    added settings to serve static images in development environment to blog/urls.py

--- yadblog/blog/urls.py
+++ yadblog/blog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('', views.PostListView.as_view(), name='post-list'),
     path('<slug:slug>', views.PostDetailView.as_view(), name='post-detail'),
     path('<slug:slug>/<int:pk>', views.CommentReplyView.as_view(), name='comment-reply'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
