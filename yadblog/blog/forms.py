7fb3e9fc8a227ce08bdc2a63291193567b4f498a
luke biggerstaff <luke.biggerstaff@gmail.com>
2018-01-14 18:39:38 -0600

    added widgets to comment form for bootstrap styling classes and placeholder values

--- yadblog/blog/forms.py
+++ yadblog/blog/forms.py
@@ -1,8 +1,22 @@
-from django.forms import ModelForm
+from django import forms
 from .models import Comment
 
 
-class CommentForm(ModelForm):
+class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['name', 'email', 'body']
+        widgets = {
+            'name' : forms.TextInput(attrs={
+                'placeholder': 'Name',
+                'class' : 'form-control text-muted',
+            }),
+            'email' : forms.EmailInput(attrs={
+                'placeholder': 'Email',
+                'class' : 'form-control text-muted',
+            }),
+            'body' : forms.Textarea(attrs={
+                'placeholder': 'Leave Your Comment Here...',
+                'class' : 'form-control text-muted',
+            }),
+        }
