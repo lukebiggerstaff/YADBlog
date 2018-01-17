493ba45e5e8735a67cba066f715db96bcaa36467
luke biggerstaff <luke.biggerstaff@gmail.com>
2018-01-17 15:38:57 -0600

    added view to send emails from the contact form, will need to configure settings.py more fully to use this in production

--- yadblog/blog/views.py
+++ yadblog/blog/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+
 from django.utils.html import strip_tags
 from django.utils.timezone import now
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import FormView
+from django.core.mail import send_mail
+from django.conf import settings
 
 from .models import Post, Comment
 from .forms import CommentForm, ContactForm
 
 # Create your views here.
+ADMIN_EMAIL_ADDRESS = settings.ADMIN_EMAIL_ADDRESS
 
 
 class PostListView(ListView):
@@ -69,3 +73,22 @@ class CommentReplyView(DetailView, FormView):
 class ContactFormView(FormView):
     form_class = ContactForm
     template_name = "blog/contact_me.html"
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            subject = form.cleaned_data['subject']
+            message = form.cleaned_data['message']
+            message = name + ': ' + message
+
+            send_mail(
+                subject,
+                message,
+                email,
+                [ADMIN_EMAIL_ADDRESS],
+                fail_silently=False
+            )
+            return HttpResponseRedirect(reverse('post-list'))
