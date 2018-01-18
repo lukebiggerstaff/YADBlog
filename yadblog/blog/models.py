8f3e8bc7e6f9fb26fa7186dc37eca6aafba44e47
luke biggerstaff <luke.biggerstaff@gmail.com>
2018-01-18 14:46:07 -0600

    removed custom template tags to render post body as a template and instead created custom model function to return post body as a template which can be called from the view

--- yadblog/blog/models.py
+++ yadblog/blog/models.py
@@ -1,3 +1,4 @@
+from django import template
 from django.db import models
 from django.utils import timezone
 from django.urls import reverse
@@ -13,8 +14,13 @@ class Post(models.Model):
     description = models.TextField(blank=True)
     slug = models.SlugField()
 
+    @property
+    def body_template(self):
+        return template.Template(self.body)
+
     class Meta:
         ordering = ['-published_date',]
+
 
     def __str__(self):
         return '{}'.format(self.title)
