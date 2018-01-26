2333d8906297b998ffb8abc193200250c756879d
luke biggerstaff <luke.biggerstaff@gmail.com>
2018-01-26 10:04:46 -0600

    added selenium tests to check for links and usage of comment form on detail page

---
+++ yadblog/blog/selenium_tests/test_detail_page.py
@@ -0,0 +1,75 @@
+import time
+
+from selenium import webdriver
+
+from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+
+
+class TestDetailPage(StaticLiveServerTestCase):
+    fixtures = ['blog_views_testdata.json']
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def submit_comment(self, reply_data):
+        name_input = self.browser.find_element_by_id('id_name')
+        name_input.send_keys(reply_data['name'])
+
+        email_input = self.browser.find_element_by_id('id_email')
+        email_input.send_keys(reply_data['email'])
+
+        body_input = self.browser.find_element_by_id('id_body')
+        body_input.send_keys(reply_data['body'])
+
+        reply_button = self.browser.find_element_by_id('id_submit')
+        reply_button.click()
+
+
+    def test_home_page_links_to_detail_page(self):
+        self.browser.get(self.live_server_url)
+
+        # grab headline links from post in fixture data
+        fixture_link_list = self.browser.find_elements_by_partial_link_text(
+            'Post Should'
+        )
+        post_links = dict()
+        for link in fixture_link_list:
+            link_text = link.text
+            link_url = link.get_attribute('href')
+            post_links[link_url] = link_text
+        for link in post_links.keys():
+            self.browser.get(link)
+            headline = self.browser.find_element_by_class_name('main-heading').text
+            self.assertEqual(post_links[link], headline)
+
+    def test_detail_page_comment_form_can_submit(self):
+        self.browser.get(self.live_server_url + '/post-should-be-visible-1#comments')
+        reply_data = {
+            'name' : 'selenium user 1',
+            'email' : 'test3@test.com',
+            'body' : 'selenium comment #1'
+        }
+        name_input = self.browser.find_element_by_id('id_name')
+        name_input.send_keys(reply_data['name'])
+
+        email_input = self.browser.find_element_by_id('id_email')
+        email_input.send_keys(reply_data['email'])
+
+        body_input = self.browser.find_element_by_id('id_body')
+        body_input.send_keys(reply_data['body'])
+
+        reply_button = self.browser.find_element_by_id('id_submit')
+        reply_button.click()
+
+        comment_name = self.browser.find_element_by_css_selector(
+            '.parent-comment:last-of-type>h4'
+        ).text
+        comment_body = self.browser.find_element_by_css_selector(
+            '.parent-comment:last-of-type>p'
+        ).text
+        self.assertEquals(reply_data['name'], comment_name)
+        self.assertEquals(reply_data['body'], comment_body)
